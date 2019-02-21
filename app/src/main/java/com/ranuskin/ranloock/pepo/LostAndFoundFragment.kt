a1e8fb0af1ebac81c9f9b07dcda8c4a8497136aa
Ran Loock <ran.trisk@gmail.com>
2019-02-21 12:00:00 +0200

    maps first

---
+++ app/src/main/java/com/ranuskin/ranloock/pepo/LostAndFoundFragment.kt
@@ -0,0 +1,91 @@
+package com.ranuskin.ranloock.pepo
+
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.*
+import com.google.android.gms.tasks.Task
+
+
+class LostAndFoundFragment : Fragment(), OnMapReadyCallback {
+
+    var mFusedLocationProviderClient: FusedLocationProviderClient? = null
+    var mLastKnownLocation: Location? = null
+    var mMap: GoogleMap? = null
+    private var mLocationPermissionGranted = true
+
+    override fun onMapReady(p0: GoogleMap?) {
+
+        mMap = p0
+
+        if (ContextCompat.checkSelfPermission(activity!!,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = false
+            return
+        }
+
+        p0!!.isMyLocationEnabled = true
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_lost_and_found_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(activity!!)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.lost_and_found_map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+    }
+
+    private fun getLastKnownLocation(){
+        Log.d("meow", "getLastKnownLocation was called")
+        if (ContextCompat.checkSelfPermission(activity!!,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        mFusedLocationProviderClient?.lastLocation?.addOnCompleteListener(activity!!
+        ) { task: Task<Location> ->
+            if (task.isSuccessful){
+                mLastKnownLocation = task.result
+        }
+    }
+    }
+
+    private fun updateLocationUI() {
+        if (mMap == null) {
+            return
+        }
+        try {
+            if (mLocationPermissionGranted) {
+                mMap?.isMyLocationEnabled = true
+                mMap?.uiSettings?.isMyLocationButtonEnabled = true
+            } else {
+                mMap?.isMyLocationEnabled = false
+                mMap?.getUiSettings()?.isMyLocationButtonEnabled = false
+                mLastKnownLocation = null
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message)
+        }
+
+    }
+
+}
--- app/src/main/java/com/ranuskin/ranloock/pepo/MainActivity.kt
+++ app/src/main/java/com/ranuskin/ranloock/pepo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ranuskin.ranloock.pepo
 
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
@@ -9,13 +10,45 @@ import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.support.annotation.NonNull
+import android.widget.Toast
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.common.ConnectionResult
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.location.LocationManager
+import android.content.Context.LOCATION_SERVICE
+import android.support.v4.content.ContextCompat.getSystemService
+import android.content.DialogInterface
+import android.support.v4.app.ActivityCompat.startActivityForResult
+import android.support.v4.app.FragmentActivity
+import android.support.v7.app.AlertDialog
+import android.util.Log
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapFragment
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import android.Manifest
+
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private val TAG = "MainActivity"
+
+    var mLocationPermissionGranted = false
+    val ERROR_DIALOG_REQUEST = 9001
+    val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 9002
+    val PERMISSIONS_REQUEST_ENABLE_GPS = 9003
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
 
         val toggle = ActionBarDrawerToggle(
             this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
@@ -24,9 +57,124 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
-        supportFragmentManager.beginTransaction().replace(R.id.container, MapFragment()).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.container, LostAndFoundFragment()).commit()
     }
 
+
+
+    private fun checkMapServices(): Boolean {
+        if (isServicesOK()) {
+            if (isMapsEnabled()) {
+                return true
+            }
+        }
+        return false
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (checkMapServices()){
+            if (mLocationPermissionGranted){
+
+            } else {
+                getLocationPermission()
+            }
+        }
+    }
+
+    private fun buildAlertMessageNoGps() {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("This application requires GPS to work properly, do you want to enable it?")
+            .setCancelable(false)
+            .setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, id ->
+                val enableGpsIntent = Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivityForResult(enableGpsIntent, PERMISSIONS_REQUEST_ENABLE_GPS)
+            })
+        val alert = builder.create()
+        alert.show()
+    }
+
+    fun isMapsEnabled(): Boolean {
+        val manager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps()
+            return false
+        }
+        return true
+    }
+
+    private fun getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(
+                this.applicationContext,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            mLocationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
+    fun isServicesOK(): Boolean {
+        //Log.d(FragmentActivity.TAG, "isServicesOK: checking google services version")
+
+        val available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this@MainActivity)
+
+        if (available == ConnectionResult.SUCCESS) {
+            //everything is fine and the user can make map requests
+
+            return true
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            //an error occured but we can resolve it
+            val dialog =
+                GoogleApiAvailability.getInstance().getErrorDialog(this@MainActivity, available, ERROR_DIALOG_REQUEST)
+            dialog.show()
+        } else {
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show()
+        }
+        return false
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        mLocationPermissionGranted = false
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ENABLE_GPS -> {
+                if (mLocationPermissionGranted) {
+
+                } else {
+                    getLocationPermission()
+                }
+            }
+        }
+
+    }
     override fun onBackPressed() {
         if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
             drawer_layout.closeDrawer(GravityCompat.START)
